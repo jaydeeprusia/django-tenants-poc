b7d4d6b38704bf7b34cc893724dbccac0a619e7f
jaydeep.rusia <jaydeeprusia@gmail.com>
2025-02-22 23:26:11 +0530

    Initial commit

---
+++ home/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from users.views import profile_view
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('', include('home.urls')),
+    path('profile/', include('users.urls')),
+    path('@<username>/', profile_view, name="profile"),
+]
+
+# Only used when DEBUG=True, whitenoise can serve files when DEBUG=False
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
